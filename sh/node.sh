8c88541c2fbb909e3b6d73f9bc09b8b6e48e39f1
vargaLaszlo <laszlo.varga@jaystack.com>
2016-05-11 16:58:15 +0200

    Bootstrap, node

--- sh/node.sh
+++ sh/node.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Install Node.js, npm
+# Install Node.js, npm via package manager
 
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-
 sudo apt-fast -q -y --force-yes install nodejs
 
 # Install nvm
@@ -15,6 +14,11 @@ sudo chmod 777 -R .nvm
 
 echo 'export NVM_DIR="/home/vagrant/.nvm"' >> /home/vagrant/.bashrc
 echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm' >> /home/vagrant/.bashrc
+
+# Install Node.js, npm via nvm
+
+#nvm install 4.4.4
+#nvm alias default 4.4.4
 
 # Print versions
 
