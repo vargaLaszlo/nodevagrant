61eaecfa3d6fdb584e5be5d2af1d2758c146710d
vargaLaszlo <laszlo.varga@jaystack.com>
2016-05-12 12:04:57 +0200

    Global npm install without sudo

--- sh/bootstrap.sh
+++ sh/bootstrap.sh
@@ -10,6 +10,11 @@ sudo apt-fast -q -y --force-yes install git wget curl build-essential libssl-dev
 # settings
 
 git config --global credential.helper 'cache --timeout=6000'
+
 chmod +x /home/vagrant/sh/*
+
 echo 'alias npmi="npm install --no-bin-link"' >> /home/vagrant/.bashrc
 echo 'alias npm="npm --no-bin-link"' >> /home/vagrant/.bashrc
+
+echo 'sudo chmod -R 777 /home/vagrant/.nvm' >> /home/vagrant/.bashrc
+echo 'sudo chmod -R 777 /home/vagrant/.npm' >> /home/vagrant/.bashrc
